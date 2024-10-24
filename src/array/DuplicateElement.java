cd868ce91ba65165c6881d8d9ed89304b7e382ea
yogiklive <63633389+yogiklive@users.noreply.github.com>
2024-10-24 12:25:36 +0530

    Finding duplicate in Array - 3 Best ways

--- src/array/DuplicateElement.java
+++ src/array/DuplicateElement.java
@@ -1,18 +1,22 @@
 package array;
 
+import java.util.HashSet;
+
 public class DuplicateElement {
 
 	public static void main(String args[]) {
 
 		DuplicateElement duplicateElement = new DuplicateElement();
-		duplicateElement.findDuplicate();
-		// duplicateElement.findDuplicateOptimised();
+		duplicateElement.findDuplicateBruteForce();
+		duplicateElement.findDuplicateUsingAuxillaryArray();
+		duplicateElement.findDupicatesUsingHashSet();
 
 	}
-	
-	private void findDuplicate() {
 
-		int arr[] = { 5, 3, 7, 2, 3, 4, 2 };
+	// O(n^2)
+	private void findDuplicateBruteForce() {
+
+		int arr[] = { 7, 5, 3, 7, 2, 3, 4, 2 };
 
 		for (int i = 0; i < arr.length; i++) {
 
@@ -26,33 +30,42 @@ public class DuplicateElement {
 		}
 	}
 
-	private void findDuplicateOptimised() {
+	// O(n)
+	// add one in second array for index postion of each element in first array
+	// This works only if the elements value in the array is less the value of
+	// size of the array
+	private void findDuplicateUsingAuxillaryArray() {
 
-		int arr[] = { 5, 7, 2, 3, 9, 7, 4, 3 };
+		int[] arr = { 2, 1, 3, 4, 1 };
 
-		int arr1[] = new int[arr.length];
+		int[] arr1 = new int[arr.length];
 
 		for (int i = 0; i < arr.length; i++) {
 
-			if (i == 0) {
-				arr1[i] = arr[i];
+			if (arr1[arr[i]] == 1) {
+				System.out.println(" Duplicate element is " + arr[i]);
 			} else {
 
-				for (int j = 0; j < arr1.length; j++) {
+				arr1[arr[i]] = arr1[arr[i]] + 1;
+			}
+		}
+	}
 
-					if (arr[i] == arr1[j]) {
-						System.out.println("Duplicate element is " + arr[i]);
-					} else {
-						arr1[j] = arr[i];
-					}
+	private void findDupicatesUsingHashSet() {
 
-				}
+		int[] arr = { 52, 78, 21, 12, 78, 5 };
 
+		HashSet<Integer> set = new HashSet<Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (set.contains(arr[i])) {
+				System.out.println("Duplicate Element is " + arr[i]);
+			} else {
+				set.add(arr[i]);
 			}
 		}
 
 	}
-
-
 
 }
